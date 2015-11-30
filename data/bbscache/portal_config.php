45d6e159b93b4afb2b4e1cd5c7fcf4f56eb6abd6
Jiera <zyue0119@163.com>
2015-11-30 16:00:56 +0800

    Initial commit

---
+++ data/bbscache/portal_config.php
@@ -0,0 +1,6 @@
+<?php
+$portal_staticstate=array(
+	'groupgatherleft' => '0',
+	'groupgatherright' => '0',
+);
+?>
---
+++ data/bbscache/portalhtml_config.php
@@ -0,0 +1,6 @@
+<?php
+$portalhtml_times=array(
+	'groupgatherleft' => '1448856216',
+	'groupgatherright' => '1448856216',
+);
+?>
